f5f4bb443ca59f203483e28032189a69d5ee214b
WatermeIonn <daniellukecooper@outlook.com>
2023-09-17 15:47:45 +0100

    feat: Added GET, POST and DELETE endpoints to Goal API.

--- pages/api/goal.ts
+++ pages/api/goal.ts
@@ -1,15 +1,36 @@
-import { Goal } from "@/models/goal.model";
-import { PrismaClient } from "@prisma/client";
-import { plainToClass } from "class-transformer";
+import { ErrorResponseInterface } from "@/interfaces/ErrorResponseInterface";
+import { Goal, PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse<Goal | Goal[]>
+  req: NextApiRequest,
+  res: NextApiResponse<Goal | Goal[] | ErrorResponseInterface>
 ) {
-  const result = await prisma.goal.findMany();
+  let result: Goal | Goal[];
 
-  res.status(200).json(result.map((goal) => plainToClass(Goal, goal)));
+  try {
+    switch (req.method) {
+      case "GET":
+        result = await prisma.goal.findMany();
+        return res.status(200).json(result);
+      case "POST":
+        result = await prisma.goal.create({ data: req.body });
+        return res.status(200).json(result);
+      case "DELETE":
+        const id = req.query.id;
+        if (!id || Array.isArray(id)) {
+          return res.status(400).json({ message: "Id of the record to delete must be provided" });
+        }
+
+        result = await prisma.goal.delete({ where: { id } });
+        return res.status(200).json(result);
+      default:
+        throw Error(`Unrecognised HTTP method ${req.method}.`);
+    }
+  } catch (e) {
+    console.error("Encountered error in Goal API:", e);
+    res.status(500).json({ message: "Encountered unkown error" });
+  }
 }
