58117b9d8b19ceabe7d043bafce92fd13fdcd09f
dco <daniel.cooper@oxbury.com>
2025-09-17 14:04:21 +1200

    fix: Fix positioning of theme toggle button that caused the main page to be shifted to the left

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,19 @@
-import { fontSans } from "@/config/fonts";
-import "@/styles/globals.css";
-import clsx from "clsx";
-import { Providers } from "./providers";
-import { ThemeToggle } from "@/components/themeToggle";
+import { fontSans } from '@/config/fonts';
+import '@/styles/globals.css';
+import clsx from 'clsx';
+import { Providers } from './providers';
+import { ThemeToggle } from '@/components/themeToggle';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={clsx(
-          "bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+      <body className={clsx('bg-background font-sans antialiased', fontSans.variable)}>
         <Providers>
-        <ThemeToggle />
           <div className="relative flex flex-col pt-10">
-            <main className="container mx-auto max-w-8xl px-6 flex-grow">
-              {children}
-            </main>
+            <main className="container mx-auto max-w-8xl px-6 flex-grow">{children}</main>
           </div>
+          <ThemeToggle />
         </Providers>
       </body>
     </html>
--- components/themeToggle.tsx
+++ components/themeToggle.tsx
@@ -21,7 +21,7 @@ export function ThemeToggle() {
   return (
     <button 
       onClick={toggleTheme}
-      className="float-right mt-5 mr-5 rounded-full border-3 text-gray-700 border-gray-700 p-2 hover:text-gray-900 hover:border-gray-900 dark:text-gray-200 dark:border-gray-200 dark:hover:text-gray-400 dark:hover:border-gray-400 transition-colors"
+      className="absolute top-5 right-5 rounded-full border-3 text-gray-700 border-gray-700 p-2 hover:text-gray-900 hover:border-gray-900 dark:text-gray-200 dark:border-gray-200 dark:hover:text-gray-400 dark:hover:border-gray-400 transition-colors"
       aria-label="Toggle theme"
     >
       {theme === "dark" ? <IconSunFilled /> : <IconMoonFilled />}
